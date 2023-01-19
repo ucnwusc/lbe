0ccff40cb5bc6e9bf4ea1abc17ba9ffe3195f0b7
ucnwusc <zhou@uchicago.edu>
2023-01-19 01:26:21 -0800

    add if.c for if_nameindex.c

---
+++ c/src/if.c
@@ -0,0 +1,23 @@
+#include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    struct if_nameindex *if_ni, *i;
+
+    if_ni = if_nameindex();
+    if (if_ni == NULL) {
+        perror("if_nameindex");
+        exit(EXIT_FAILURE);
+   }
+
+   for (i = if_ni; ! (i->if_index == 0 && i->if_name == NULL); i++)
+       printf("%u: %s\n", i->if_index, i->if_name);
+
+      if_freenameindex(if_ni);
+
+      exit(EXIT_SUCCESS);
+}
+//https://man7.org/linux/man-pages/man3/if_nameindex.3.html
